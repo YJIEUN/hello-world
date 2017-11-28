ba95cbca5eb6f2bf2ed7e6772cf76dfc19c13a6a
Administrator <Administrator@MSDN-SPECIAL>
2017-11-28 21:54:50 +0900

    jieun을 JIEUN으로 바꿈

--- src/helloworld.java
+++ src/helloworld.java
@@ -2,6 +2,6 @@
 public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		System.out.println("hello jieun");
+		System.out.println("hello JIEUN");
 			}
 }
